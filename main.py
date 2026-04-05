c934ca3d15e574c157b181f1464bfa3a3b920c3e
Joe Swisher <jswisher6@ivytech.edu>
2026-04-05 12:15:05 -0400

    added check for doors and roof input

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ class Automobile(Vehicle):
     make = ""
     model = ""
     doors = "" # 2 or 4. also a string; will not be used in math
+    valid_doors = ("2", "4")
     roof = "" # solid or sun roof
+    valid_roof = ("solid", "sun roof")
 
 print("Hello. Please give me some details about a car to test my Automobile class.")
 Vehicle.vehicle_type = "car"
@@ -16,8 +18,19 @@ user_car = Automobile()
 user_car.year = input("What year was the car made? ")
 user_car.make = input("What make is the car? (e.g. Toyota, Ford) ")
 user_car.model = input("What model is the car? (e.g. Corolla, Mustang) ")
-user_car.doors = input("How many doors does the car have? ")
-user_car.roof = input("What type of roof does the car have? (solid or sun roof) ")
+
+while user_car.doors not in user_car.valid_doors:
+    user_car.doors = input("How many doors does the car have? (Enter 2 or 4) ").strip()
+    if user_car.doors not in user_car.valid_doors:
+        print("Please input a valid number (2 or 4).")
+
+while user_car.roof not in user_car.valid_roof:
+    user_car.roof = input("What type of roof does the car have? (Enter solid or sun roof) ").strip().lower()
+    if user_car.roof not in user_car.valid_roof:
+        print("Please input a valid roof (solid or sun roof)") # I would probably change this 
+                                                               # to use Automobile.valid_roof in case more types of roofs 
+                                                               # are added later in a real program. Same with the valid_doors one above.
+
 print()
 print(f"Vehicle type: {user_car.vehicle_type}")
 print(f"Year: {user_car.year}")
